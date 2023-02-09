60733c14c73c5b5a532565ddf7bb65a7ec942a06
ChRamijuddin <chramijuddin@gmail.com>
2023-02-09 21:56:01 +0530

    booking app

---
+++ api/routes/user.route.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { deleteUser, getUser, getUsers, updateUser } from '../controllers/user.controller.js';
+import { verifyAdmin, verifyUser } from '../utils/verifyTokens.js';
+const router = express.Router();
+
+
+//UPDATE
+router.put("/:id", verifyUser, updateUser);
+
+//DELETE
+router.delete("/:id", verifyUser, deleteUser);
+
+//GET
+router.get("/:id", verifyUser, getUser);
+
+//GET ALL
+router.get("/", verifyAdmin, getUsers);
+
+export default router
